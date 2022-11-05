c70c0b2fdcc0367934a5cbc8be2b0bfc61d40629
Ryan Sweny <ryan@bitbakery.co>
2022-11-05 16:41:30 -0400

    add peak total fee per time period

--- src/scheduled/check-gas/index.js
+++ src/scheduled/check-gas/index.js
@@ -31,12 +31,13 @@ exports.handler = async function scheduled (event) {
       const n = result.Items[0].count + 1;
       const oldAvg = result.Items[0].avg;
       const oldAvgTip = result.Items[0].avgTip;
-      const high = Math.max(result.Items[0].high, Math.round(curGasFee));
-      const low = Math.min(result.Items[0].low, Math.round(curGasFee));
+      const high = Math.max(result.Items[0].high, Math.round(curBaseFee));
+      const peak = Math.max(result.Items[0].peak, Math.round(curGasFee));
+      const low = Math.min(result.Items[0].low, Math.round(curBaseFee));
 
       // once per day write a daily summary entry
       if (hour == 23 && n == 50) {
-        await writeDaySummary(db, d, today)
+        await writeDaySummary(db, d, today);
       }
 
       return db.ethgas.put({
@@ -45,7 +46,8 @@ exports.handler = async function scheduled (event) {
         count: n,
         high,
         low,
-        avg: Math.round( curGasFee/n + oldAvg*(n-1)/n ),
+        peak,
+        avg: Math.round( curBaseFee/n + oldAvg*(n-1)/n ),
         avgTip: Math.round( minerTip/n + oldAvgTip*(n-1)/n )
       });
     } 
@@ -55,9 +57,10 @@ exports.handler = async function scheduled (event) {
         pk: today,
         sk: String(hour),
         count: 1,
-        high: curGasFee,
-        low: curGasFee,
-        avg: curGasFee,
+        high: Math.round(curBaseFee),
+        low: Math.round(curBaseFee),
+        peak: curGasFee,
+        avg: curBaseFee,
         avgTip: minerTip,
       });
     }
@@ -68,10 +71,10 @@ async function writeDaySummary(db, d, today) {
   const year = d.getUTCFullYear();
   const summaryKey = `sum-${year}`;
   let summaryResult = await db.ethgas.query({
-      KeyConditionExpression: 'pk = :today',
-      ExpressionAttributeValues: {
-        ':today': today,
-      }
+    KeyConditionExpression: 'pk = :today',
+    ExpressionAttributeValues: {
+      ':today': today,
+    }
   });
 
   console.log(summaryResult.Items);
@@ -79,10 +82,12 @@ async function writeDaySummary(db, d, today) {
   let sumAvgTip = 0;
   let sumHigh = 0;
   let sumLow = 100000;
+  let peak = 0;
   const numHours = summaryResult.Items.length;
   for (let i = 0; i < numHours; i++) {
     sumLow = Math.min(sumLow, summaryResult.Items[i].low);
     sumHigh = Math.max(sumHigh, summaryResult.Items[i].high);
+    if (summaryResult.Items[i].peak > peak) peak = summaryResult.Items[i].peak;
     sumAvg += summaryResult.Items[i].avg;
     sumAvgTip += summaryResult.Items[i].avgTip;
   }
@@ -96,5 +101,6 @@ async function writeDaySummary(db, d, today) {
     low: sumLow,
     avg: sumAvg,
     avgTip: sumAvgTip,
+    peak
   });
 } 
--- src/scheduled/deploy-s3/index.js
+++ src/scheduled/deploy-s3/index.js
@@ -6,52 +6,52 @@ let { S3_BUCKET } = require('@architect/shared/constants');
 exports.handler = async function scheduled (event) {
   const gasData = {};
   const today = new Date();
-  const year = today.getUTCFullYear();
   const hour = today.getUTCHours();
   gasData.lastUpdated = today.toISOString();
+  
+  // populate and upload the last 7 days by hour
   for (let i = 0; i < 24; i++) {
     gasData[i] = [];
     for (let j = 0; j < 7; j++) {
-       gasData[i].push({ a: 0, l: 0, h: 0, f: 0 });
+       gasData[i].push({ a: 0, l: 0, h: 0, f: 0, p: 0 });
     }
   }
-
   let db = await arc.tables();
   for (let i = 0; i < 7; i++) {
-    await populateWeek(db, gasData, i);
+    await populateWeekday(db, gasData, i);
   }
+  const codeStr = `const gasYearData = {};\nconst gasData = ${JSON.stringify(gasData)};\n`;
+  await uploadFileToS3(S3_BUCKET, 'gasdata.js', Buffer.from(codeStr));
   
-  // upload daily summary data
-  if (hour > 22)
-  {
+  if (hour > 22) {
+    // upload yearly summary data by day
+    const year = today.getUTCFullYear();
     const gasYearData = await populateYear(db, year);
     const codeYearStr = `gasYearData[${year}] = ${JSON.stringify(gasYearData)};\n`;
     await uploadFileToS3(S3_BUCKET, `gasyeardata${year}.js`, Buffer.from(codeYearStr));
 
+    // upload daily summary data by hour
     const dateStr = today.toISOString().substring(0,10);
+
     const gasDayData = await populateDay(db, dateStr);
     const codeDayStr = JSON.stringify(gasDayData);
     await uploadFileToS3(S3_BUCKET, `${year}/${dateStr}.json`, Buffer.from(codeDayStr));
   }
-
-  // upload hourly data
-  const codeStr = `const gasYearData = {};\nconst gasData = ${JSON.stringify(gasData)};\n`;
-  return uploadFileToS3(S3_BUCKET, 'gasdata.js', Buffer.from(codeStr));
 }
 
-async function populateWeek(db, gasData, daysAgoIndex) {
+async function populateWeekday(db, gasData, daysAgoIndex) {
   try {
     const d = new Date();
     d.setTime(d.getTime() - 86400*1000*daysAgoIndex);
     const dateStr = d.toISOString().substring(0,10);
     let res = await db.ethgas.query({
-        KeyConditionExpression: 'pk = :dateStr',
-        ExpressionAttributeValues: {
-          ':dateStr': dateStr,
-        }
+      KeyConditionExpression: 'pk = :dateStr',
+      ExpressionAttributeValues: {
+        ':dateStr': dateStr,
+      }
     });
     for (let i = 0; i < res.Items.length; i++) {
-      gasData[res.Items[i].sk][6-daysAgoIndex] = { a: res.Items[i].avg, l: res.Items[i].low, h: res.Items[i].high, f: res.Items[i].avgTip };
+      gasData[res.Items[i].sk][6-daysAgoIndex] = { a: res.Items[i].avg, l: res.Items[i].low, h: res.Items[i].high, f: res.Items[i].avgTip, p: res.Items[i].peak };
     }
   } catch(e) {
     console.log('populateDay Error');
@@ -64,13 +64,13 @@ async function populateDay(db, dateStr) {
   try {
     
     let res = await db.ethgas.query({
-        KeyConditionExpression: 'pk = :dateStr',
-        ExpressionAttributeValues: {
-          ':dateStr': dateStr,
-        }
+      KeyConditionExpression: 'pk = :dateStr',
+      ExpressionAttributeValues: {
+        ':dateStr': dateStr,
+      }
     });
     for (let i = 0; i < res.Items.length; i++) {
-      gasDayData[res.Items[i].sk] = { a: res.Items[i].avg, l: res.Items[i].low, h: res.Items[i].high, f: res.Items[i].avgTip };
+      gasDayData[res.Items[i].sk] = { a: res.Items[i].avg, l: res.Items[i].low, h: res.Items[i].high, f: res.Items[i].avgTip, p: res.Items[i].peak };
     }
   } catch(e) {
     console.log('populateDay Error');
@@ -84,10 +84,10 @@ async function populateYear(db, year) {
   try {
     const yr = `sum-${year}`;
     let res = await db.ethgas.query({
-        KeyConditionExpression: 'pk = :yr',
-        ExpressionAttributeValues: {
-          ':yr': yr,
-        }
+      KeyConditionExpression: 'pk = :yr',
+      ExpressionAttributeValues: {
+        ':yr': yr,
+      }
     });
 
     for (let i = 0; i < res.Items.length; i++) {
@@ -95,7 +95,7 @@ async function populateYear(db, year) {
       const month = parseInt(d.substring(5,7));
       const day = parseInt(d.substring(8));
       if (!gasYearData[month]) gasYearData[month] = {};
-      gasYearData[month][day] = { d: res.Items[i].sk, a: res.Items[i].avg, l: res.Items[i].low, h: res.Items[i].high, f: res.Items[i].avgTip };
+      gasYearData[month][day] = { d: res.Items[i].sk, a: res.Items[i].avg, l: res.Items[i].low, h: res.Items[i].high, f: res.Items[i].avgTip, p: res.Items[i].peak };
     }
   } catch(e) {
     console.log('populateYear Error');
@@ -113,11 +113,11 @@ async function uploadFileToS3(bucket, key, data) {
   }
 
   try {
-      const response = await s3.upload(params).promise();
-      console.log('S3 Response: ', response);
-      return response;
+    const response = await s3.upload(params).promise();
+    console.log('S3 Response: ', response);
+    return response;
   } catch (err) {
-      console.log(err, err.stack);
-      return err;
+    console.log(err, err.stack);
+    return err;
   }
 }
